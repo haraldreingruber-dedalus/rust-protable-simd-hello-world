088905f66a7ea39288facf5f5c5fc51ea37f8e03
haraldreingruber-dedalus <74898239+haraldreingruber-dedalus@users.noreply.github.com>
2021-02-03 17:37:43 +0100

    Add `cargo build` to script

--- build.cmd
+++ build.cmd
@@ -1,2 +1,4 @@
+cargo build
+
 REM Use --debug if you want to verify the simd instructions in the disassembly. In the release version the const computation is optimized quite a bit :)
 wasm-pack build . --target web --debug
